2e10cc5a9ec5ca9fc631e9fbee8ef8ebea137431
Jean Carlierm <jean.carlier@gmail.com>
2026-07-07 17:23:14 -0600

    Finish development of /status endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,13 @@
 test("GET /api/v1/status should return status 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
     expect(response.status).toBe(200);
+    
     const data = await response.json();
-    expect(data).toEqual({ status: "Parabens" });
+    const parsedUpdatedAt = new Date(data.updated_At).toISOString();
+    expect(data.updated_At).toBe(parsedUpdatedAt);
+
+    expect(data.dependecies.database.version).toEqual("16.0");
+    expect(data.dependecies.database.max_connections).toEqual("100");
+    expect(data.dependecies.database.current_connections).toEqual(1);
 });
+
