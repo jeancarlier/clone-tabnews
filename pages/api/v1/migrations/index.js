0a0d8b3a35b10b483fb3cd83905b33ebbac05c65
Jean Carlierm <jean.carlier@gmail.com>
2026-07-10 17:00:29 -0600

    rolling back fix

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -36,7 +36,5 @@ export default async function migrations(request, response) {
         return response.status(200).json(migratedMigrations);
     }
 
-    await dbClient.end();
-
     return response.status(405).json({ error: "Method not allowed" });
 }
