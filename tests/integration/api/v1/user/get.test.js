e8e34d466a2e2788177ee193483b9adbf5343b6d
Jean Carlierm <jean.carlier@gmail.com>
2026-08-25 14:44:36 -0600

    fix: add `Cache-Control` to `api/v1/user` endpoint response to force checking the renewed session

--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -27,6 +27,12 @@ describe("GET /api/v1/user", () => {
       });
       expect(response.status).toBe(200);
 
+      const cacheControl = response.headers.get("Cache-Control");
+      console.log(cacheControl);
+      expect(cacheControl).toBe(
+        "no-store, no-cache, max-age=0, must-revalidate",
+      );
+
       const responseBody = await response.json();
       expect(responseBody).toEqual({
         id: createdUser.id,
@@ -131,6 +137,12 @@ describe("GET /api/v1/user", () => {
         },
       });
       expect(response.status).toBe(200);
+
+      const cacheControl = response.headers.get("Cache-Control");
+      console.log(cacheControl);
+      expect(cacheControl).toBe(
+        "no-store, no-cache, max-age=0, must-revalidate",
+      );
 
       // Session renewal assertions
       const renewedSession = await session.findOneValidByToken(
