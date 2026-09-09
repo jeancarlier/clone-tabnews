998d579326517a0817f59c483ac74fa176dc2521
Jean Carlierm <jean.carlier@gmail.com>
2026-09-09 10:37:55 -0600

    refactor: improve error logging in `email.send`

--- infra/email.js
+++ infra/email.js
@@ -1,4 +1,5 @@
 import nodemailer from "nodemailer";
+import { ServiceError } from "errors.js";
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SMTP_HOST,
@@ -11,7 +12,17 @@ const transporter = nodemailer.createTransport({
 });
 
 async function send(mailOptions) {
-  await transporter.sendMail(mailOptions);
+  try {
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    throw new ServiceError({
+      message: "Falha ao enviar e-mail.",
+      action:
+        "Verifique se o serviço de email está ativo e se as credenciais estão corretas.",
+      cause: error,
+      context: mailOptions,
+    });
+  }
 }
 
 const email = {
--- infra/errors.js
+++ infra/errors.js
@@ -38,13 +38,15 @@ export class MethodNotAllowedError extends Error {
 }
 
 export class ServiceError extends Error {
-  constructor({ cause, message }) {
+  constructor({ cause, message, action, context }) {
     super(message || "Serviço indisponível no momento.", {
       cause,
     });
     this.name = "ServiceError";
-    this.action = "Verifique se o serviço está disponível e tente novamente.";
+    this.action =
+      action || "Verifique se o serviço está disponível e tente novamente.";
     this.statusCode = 503;
+    this.context = context;
   }
 
   toJSON() {
@@ -53,6 +55,7 @@ export class ServiceError extends Error {
       message: this.message,
       action: this.action,
       statusCode: this.statusCode,
+      context: this.context,
     };
   }
 }
