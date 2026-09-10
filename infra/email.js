dc92a98c736cf3b019d573d015029008ceba2f39
Jean Carlierm <jean.carlier@gmail.com>
2026-09-10 12:39:47 -0600

    fix: fix import of `ServiceError` in `email.js`

--- infra/email.js
+++ infra/email.js
@@ -1,5 +1,5 @@
 import nodemailer from "nodemailer";
-import { ServiceError } from "errors.js";
+import { ServiceError } from "infra/errors.js";
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SMTP_HOST,
